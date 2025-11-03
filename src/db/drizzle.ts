b0748731b249bdeac12a6c1b5ff6df59180323b1
sushantg <sushantg@vellestudio.com>
2025-11-03 20:53:00 +0530

    08: database

---
+++ src/db/drizzle.ts
@@ -0,0 +1,10 @@
+import { neon } from "@neondatabase/serverless";
+import { config } from "dotenv";
+import { drizzle } from "drizzle-orm/neon-http";
+
+config({ path: ".env" }); // or .env.local
+
+const sql = neon(process.env.DATABASE_URL!);
+const db = drizzle({ client: sql });
+
+export { db };
---
+++ src/db/schema.ts
@@ -0,0 +1,10 @@
+import { pgTable, serial, text } from "drizzle-orm/pg-core";
+
+export const courses = pgTable("courses", {
+  id: serial("id").primaryKey(),
+  title: text("title").notNull(),
+  imageSrc: text("image_src").notNull(),
+});
+
+export type Courses = typeof courses.$inferSelect;
+
