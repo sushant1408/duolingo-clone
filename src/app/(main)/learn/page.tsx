c0aeb3220c357cb25b099ab093454ddb87f46d8f
sushantg <sushantg@vellestudio.com>
2025-11-03 20:17:52 +0530

    07: /learn page wrappers

--- src/app/(main)/learn/page.tsx
+++ src/app/(main)/learn/page.tsx
@@ -1,5 +1,25 @@
+import { Header } from "@/app/(main)/learn/_components/header";
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { UserProgress } from "@/components/user-progress";
+
 export default function Page() {
   return (
-    <div>Learn page</div>
+    <div className="flex flex-row-reverse gap-12 px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{
+            title: "Spanish",
+            imageSrc: "/es.svg",
+          }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="Spanish" />
+      </FeedWrapper>
+    </div>
   );
 }
